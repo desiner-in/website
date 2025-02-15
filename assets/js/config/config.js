dc83a41551a1bdbb7632bcba100a7583e42c0496
Ashif Sadiq <ashifsadiq@gmail.com>
2025-02-15 19:26:58 +0530

    new Template

--- assets/js/config/config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-export default {
-    "mobile-number": `<a href="tel:+918778787943"">+91 87787 87943</a>`,
-    "mobile-number-text": `+91 87787 87943`,
-    "company-email": `<a href="mailto:contact@desiner.in">contact@desiner.in</a>`,
-    "company-email-text": `contact@desiner.in`,
-    "office-address":"Chennai, Tamil Nadu, India - 600 001"
-}
--- assets/js/script/script.js
+++
@@ -1,7 +0,0 @@
-
-import configs from "../config/config.js";
-document.addEventListener("DOMContentLoaded", () => {
-    Object.keys(configs).map((key, index) => {
-        if (document.getElementById(key)) return document.getElementById(key).innerHTML = configs[key];
-    })
-});
